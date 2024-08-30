6316b85eec7c3abb9055a293700c5fa47a2745e0
BuiiKNgann <buikimngan22130@gmail.com>
2024-08-30 14:45:17 +0700

    first commit

---
+++ server/config/dbconnect.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// Thiết lập cấu hình để chuẩn bị cho sự thay đổi trong Mongoose 7
+mongoose.set('strictQuery', false); // Hoặc true, tùy thuộc vào lựa chọn của bạn
+
+const dbConnect = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
+        if (conn.connection.readyState === 1) console.log('DB connection is successfully');
+        else console.log('DB connecting');
+
+
+
+    } catch (error) {
+        console.log('DB connection is failed');
+        throw new Error(error)
+
+    }
+}
+module.exports = dbConnect
